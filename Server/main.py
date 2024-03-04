af3b5a69d24ab2c067d3d8aaa94daa555121e7f6
Иван <ivan.boyko.2002@mail.ru>
2024-03-04 21:42:55 +0300

    modified:   Server/main.py

--- Server/main.py
+++ Server/main.py
@@ -8,8 +8,7 @@ from telebot import types
 
 dotenv_path = os.path.join(Path().absolute(), 'example.env')
 load_dotenv(dotenv_path)
-sys.path.insert(0, os.getenv('DB_PATH'))
-print(os.getenv('DB_PATH'))
+#sys.path.insert(0, os.getenv('DB_PATH'))
 from DataBase import Telegram_DB
 
 print(os.getenv('BOT_TOKEN'))
