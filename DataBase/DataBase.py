7e5197b5919dfb8517483ee6d93c172411742586
Иван <ivan.boyko.2002@mail.ru>
2024-03-04 20:40:04 +0300

    Сменил базу данных

--- DataBase/DataBase.py
+++ DataBase/DataBase.py
@@ -1,69 +1,82 @@
 # -*- coding: utf-8 -*-
-import sqlite3
+import psycopg2
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+dotenv_path = os.path.join(Path().absolute(), 'example.env')
+load_dotenv(dotenv_path)
 
 
 class Telegram_DB:
     def __init__(self):
-        self.conn = sqlite3.connect('mbt.db', check_same_thread=False)
+        db_name='test'
+        user = os.getenv('DB_USER')
+        password = os.getenv('DB_PASSWORD')
+        host= os.getenv('DB_HOST')
+        port = os.getenv('DB_PORT')
+        conn_string = "dbname='{0}' user='{1}' password='{2}' host='{3}' port='{4}'".format(db_name, user, password, host, port)
+        self.conn = psycopg2.connect(conn_string)
+    
         self.cursor = self.conn.cursor()
         self.cursor.execute('''
             CREATE TABLE IF NOT EXISTS users_data (
-                id                  INTEGER PRIMARY KEY AUTOINCREMENT,
-                Телеграм_ID         INTEGER NOT NULL,
-                Доступ              TEXT    NOT NULL
-                                    DEFAULT "Гость",
-                [Дата регистрации]  DATA    NOT NULL,
-                Статус              TEXT,
-                Рейтинг             INTEGER DEFAULT (0),
-                Заработок           INTEGER DEFAULT (0),
-                Заказы              INTEGER DEFAULT (0),
-                Комментарии         TEXT,
-                Фотография          BLOB,
-                ФИО                 TEXT    NOT NULL,
-                Пол                 TEXT    NOT NULL
-                                    CHECK (Пол = "Мужской" OR Пол = "Женский"),
-                [Дата рождения]     TEXT    NOT NULL,
-                [Место жительства]  TEXT,
-                Образование         TEXT    NOT NULL,
-                Курс                INTEGER,
-                Специальность       TEXT,
-                [Мин. ЗП]           INTEGER,
-                [Тяжелый труд]      INTEGER,
-                [Средний труд]      INTEGER,
-                [Творческий труд]   INTEGER,
-                [Иные работы]       TEXT,
-                [Рабочее время]     TEXT,
-                Инструменты         ТEXT,
-                Местный             INTEGER,
-                Языки               TEXT,
-                Телефон             TEXT    NOT NULL,
-                Водитель            TEXT,
-                Машина              INTEGER
-                Служил              INTEGER,
-                [Доп. инф.]         TEXT
-            )
+                id                  SERIAL PRIMARY KEY,
+                "Телеграм_ID"       INTEGER NOT NULL,
+                "Доступ"            TEXT NOT NULL DEFAULT 'Гость',
+                "Дата регистрации"  DATE NOT NULL,
+                "Статус"            TEXT,
+                "Рейтинг"           INTEGER DEFAULT 0,
+                "Заработок"         INTEGER DEFAULT 0,
+                "Заказы"            INTEGER DEFAULT 0,
+                "Комментарии"       TEXT,
+                "Фотография"        BYTEA,
+                "ФИО"               TEXT NOT NULL,
+                "Пол"               TEXT NOT NULL CHECK ("Пол" = 'Мужской' OR "Пол" = 'Женский'),
+                "Дата рождения"     TEXT NOT NULL,
+                "Место жительства"  TEXT,
+                "Образование"       TEXT NOT NULL,
+                "Курс"              INTEGER,
+                "Специальность"     TEXT,
+                "Мин. ЗП"           INTEGER,
+                "Тяжелый труд"      TEXT,
+                "Средний труд"      TEXT,
+                "Творческий труд"   TEXT,
+                "Иные работы"       TEXT,
+                "Рабочее время"     TEXT,
+                "Инструменты"       TEXT,
+                "Местный"           INTEGER,
+                "Языки"             TEXT,
+                "Телефон"           TEXT NOT NULL,
+                "Водитель"          TEXT,
+                "Машина"            TEXT,
+                "Служил"            TEXT,
+                "Доп. инф."         TEXT
+            );
         ''')
         self.conn.commit()
     
     def add_user(self, user_id, data_reg, photo, fio, sex, born, education_level, course, profession, min_salary, hardwork, midwork, artwork, addwork, tools, phone, residence_place):
         self.cursor.execute('''
-        INSERT INTO users_data (Телеграм_ID, [Дата регистрации], Фотография, ФИО, Пол, [Дата рождения], Образование, Курс, Специальность, [Мин. ЗП], [Тяжелый труд], [Средний труд], [Творческий труд], [Иные работы], Инструменты, Телефон, [Место жительства])
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        INSERT INTO users_data ("Телеграм_ID", "Дата регистрации", Фотография, ФИО, Пол, "Дата рождения", Образование, Курс, Специальность, "Мин. ЗП", "Тяжелый труд", "Средний труд", "Творческий труд", "Иные работы", Инструменты, Телефон, "Место жительства")
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
         ''', (user_id, data_reg, photo, fio, sex, born, education_level, course, profession, min_salary, hardwork, midwork, artwork, addwork, tools, phone, residence_place))
         self.conn.commit()
     
     def get_user_info(self, user_id):
-        result = self.cursor.execute('''
-        SELECT [Дата регистрации], Фотография, ФИО, Пол, [Дата рождения], Образование, Курс, Специальность, [Мин. ЗП], [Тяжелый труд], [Средний труд], [Творческий труд], [Иные работы], Инструменты, Телефон, [Место жительства], Заработок, Заказы FROM users_data
-        WHERE Телеграм_ID = ?
-        ''', (user_id, )).fetchone()
+        self.cursor.execute('''
+        SELECT "Дата регистрации", Фотография, ФИО, Пол, "Дата рождения", Образование, Курс, Специальность, "Мин. ЗП", "Тяжелый труд", "Средний труд", "Творческий труд", "Иные работы", Инструменты, Телефон, "Место жительства", Заработок, Заказы FROM users_data
+        WHERE "Телеграм_ID" = %s
+        ''', (user_id, ))
+        result = self.cursor.fetchone()
         return result
     
     def user_is_exist(self, user_id):
-        user = self.cursor.execute('''
+        self.cursor.execute('''
         SELECT * FROM users_data
-        WHERE Телеграм_ID = ?
-        ''', (user_id, )).fetchone()
+        WHERE "Телеграм_ID" = %s
+        ''', (user_id, ))
+        user = self.cursor.fetchone()
         if user is not None:
             return 1
         else:
@@ -72,65 +85,26 @@ class Telegram_DB:
     def edit_user(self, user_id, data_reg, photo, fio, sex, born, education_level, course, profession, min_salary, hardwork, midwork, artwork, addwork, tools, phone, residence_place):
         self.cursor.execute('''
         UPDATE users_data
-        SET [Дата регистрации] = ?, 
-            Фотография = ?, 
-            ФИО = ?, 
-            Пол = ?, 
-            [Дата рождения] = ?, 
-            Образование = ?, 
-            Курс = ?, 
-            Специальность = ?, 
-            [Мин. ЗП] = ?, 
-            [Тяжелый труд] = ?, 
-            [Средний труд] = ?, 
-            [Творческий труд] = ?, 
-            [Иные работы] = ?, 
-            Инструменты = ?, 
-            Телефон = ?, 
-            [Место жительства] = ?
-        WHERE Телеграм_ID = ?
+        SET "Дата регистрации" = %s, 
+            Фотография = %s, 
+            ФИО = %s, 
+            Пол = %s, 
+            "Дата рождения" = %s, 
+            Образование = %s, 
+            Курс = %s, 
+            Специальность = %s, 
+            "Мин. ЗП" = %s, 
+            "Тяжелый труд" = %s, 
+            "Средний труд" = %s, 
+            [Творческий труд] = %s, 
+            "Иные работы" = %s, 
+            Инструменты = %s, 
+            Телефон = %s, 
+            "Место жительства" = %s
+        WHERE "Телеграм_ID" = %s
         ''', (data_reg, photo, fio, sex, born, education_level, course, profession, min_salary, hardwork, midwork, artwork, addwork, tools, phone, residence_place, user_id))
         self.conn.commit()
         
 
-    def close(self):
-        self.conn.close()
-
-
-class Companies_DB:
-    def __init__(self):
-        self.conn = sqlite3.connect('mbt.db', check_same_thread=False)
-        self.cursor = self.conn.cursor()
-        self.create_table()
-    
-    def create_table(self):
-        # Создаем таблицу, если ее нет
-        self.cursor.execute('''
-            CREATE TABLE IF NOT EXISTS job_data (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                user_id INTEGER,
-                user_state TEXT,
-                first_name TEXT,
-                last_name TEXT,
-                age INTEGER
-            )
-        ''')
-        self.conn.commit()
-    
-    def add_user(self, user_id, user_state, first_name, last_name, age):
-        self.cursor.execute('''
-        INSERT INTO telegram_data (user_id, user_state, first_name, last_name, age)
-        VALUES (?, ?, ?, ?, ?)
-        ''', (user_id, user_state, first_name, last_name, age))
-        self.conn.commit()
-        return True
-    
-    def get_user_data(self, user_id):
-        result = self.cursor.execute('''
-        SELECT * FROM telegram_data
-        WHERE user_id = ?
-        ''', (user_id, )).fetchone()
-        return result
-    
     def close(self):
         self.conn.close()
