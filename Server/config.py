9cb6b5b1e4150e1fe6f17dc4d72184e3ff5d72f0
Иван <ivan.boyko.2002@mail.ru>
2024-06-22 16:40:05 +0300

    Добавил комментарии в config.py

--- Server/config.py
+++ Server/config.py
@@ -3,9 +3,17 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv('../config.env')
-
+# Токен бота Телеграм
 TOKEN = os.getenv('TOKEN')
+
+# URL адрес сервера базы данных
 DB_URL = os.getenv('DB_URL')
+
+# Токен доступа к БД
 DB_ACCESS_TOKEN = os.getenv('DB_ACCESS_TOKEN')
+
+# Ссылка на бота поддержки
 HELP_BOT_LINK = os.getenv('HELP_BOT_LINK')
+
+# Время до запрета отказа от заказа для пользователей
 CANCEL_HOUR_ORDER = os.getenv('CANCEL_HOUR_ORDER')
